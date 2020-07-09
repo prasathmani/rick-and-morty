d65a11644a3c7198b1e37f8d4389897454285320
Prasath Mani <prasath.mani@publicissapient.com>
2020-07-09 17:31:48 +0530

    add service workser and fix loader alignment

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Loader from 'components/Loader';
 
-import "assets/index.scss";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import Header from "components/Header";
-import Footer from "components/Footer";
-import CharacterPage from "pages/CharacterPage";
+import 'assets/index.scss';
+const Home = lazy(() => import('pages/Home'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Header = lazy(() => import('components/Header'));
+const Footer = lazy(() => import('components/Footer'));
+const CharacterPage = lazy(() => import('pages/CharacterPage'));
 
 const App = () => (
   <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/character/:characterId" component={CharacterPage} />
-      <Route component={NotFound} />
-    </Switch>
-    <Footer />
+    <Suspense fallback={<Loader />}>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/character/:characterId" component={CharacterPage} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </Suspense>
   </BrowserRouter>
 );
 
